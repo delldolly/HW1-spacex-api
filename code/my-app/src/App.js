e6d89023833220770ceb18f173a73d449fedb823
Memory <memoryeamkijkarn@gmail.com>
2021-02-17 10:34:57 +0700

    route + test API
    test axios fecth all rocket api

--- code/my-app/src/App.js
+++ code/my-app/src/App.js
@@ -1,25 +1,80 @@
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  useRouteMatch,
+  useParams
+} from "react-router-dom";
+
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Home from './Components/Home';
+import Rockets from './Components/Rockets';
+import Launches from './Components/Launches';
+
+const App = () => {
+  return(
+    
+    <Router>
+      <div class="topnav">
+        <Link to="/">Home</Link>
+        <Link to="/Rockets">Rockets</Link>
+        <Link to="/Launches">Launches</Link>
+      
+
+        
+      </div>
+      <Switch>
+          <Route path="/Rockets">
+            <Rockets />
+          </Route>
+          <Route path="/Launches">
+            <Launches />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+          
+        </Switch>
+
+    </Router>
+  )
 }
 
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
 export default App;
+{/* 
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/Rockets">Rockets</Link>
+          </li>
+          <li>
+            <Link to="/Launches">Launches</Link>
+          </li>
+        </ul> */}
---
+++ code/my-app/src/Components/Launches.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default function Launches(){
+    return(
+        <h1>Launch Detail</h1>
+    )
+} 

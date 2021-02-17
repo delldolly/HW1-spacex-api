df3556c8a704344ee648ab5c7ab610a00b1e0495
Memory <memoryeamkijkarn@gmail.com>
2021-02-18 00:06:30 +0700

    launche page filter year

--- code/my-app/src/Components/Launches.js
+++ code/my-app/src/Components/Launches.js
@@ -1,34 +1,112 @@
 import React, {useState, useEffect} from 'react';
 import axios from 'axios';
 
+import { Table } from 'reactstrap';
+
+import Select from 'react-select';
+
+import '../styles/LaunchPage.css';
+
+const options = [
+    { value: 'chocolate', label: 'Chocolate' },
+    { value: 'strawberry', label: 'Strawberry' },
+    { value: 'vanilla', label: 'Vanilla' }
+  ]
+  
 
 export default function Launches(){
-    // const response = await fetch('https://api.spacexdata.com/v3/rockets')
-    // const data = await response.json()
-    // console.log(data)
     const [data, setData] = useState([]);
-    
+    const [launchYearList, setLaunchYearList] = useState([]);
+    const [rocketNameList, setRocketNameList] = useState([]);
+    const [launchSuccessList, setLaunchSuccessList] = useState([]);
+
+    const [filterYear, setFilterYear] = useState();
     useEffect(() => {
     const fetchData = async () => {
       const result = await axios(
         // 'https://hn.algolia.com/api/v1/search?query=redux',
         'https://api.spacexdata.com/v3/Launches',
       );
-        console.log(result.data.hits)
-      setData(result.data);
+        console.log(result.data)
+        setData(result.data);
+        result.data.forEach((item) => {
+            setLaunchYearList((pre) => [...pre,{ value:  item.launch_year, label:  item.launch_year }])
+        })
+        result.data.forEach((item) => {
+            setRocketNameList((pre) => [...pre,{ value:  item.rocket.rocket_name, label:  item.rocket.rocket_name }])
+        })
+        result.data.forEach((item) => {
+            setLaunchSuccessList((pre) => [...pre,{ value:  item.launch_success, label:  item.launch_success ? "yes":"no" }])
+        })
+        
+        console.log(launchSuccessList)
+        console.log(data)
     };
- 
     fetchData();
   }, []);
-    
+
+  const handleChange = (e) => {
+    console.log("Fruit Selected!!");
+    // console.log(e.value)
+    console.log(e.value)
+    console.log(filterYear)
+    setFilterYear(e.value);
+    console.log(filterYear)
+    console.log(filterYear)
+  }
+
   return (
-    <ul>
-      {data.map(item => (
-        <li key={item.objectID}>
-            <h3>{item.mission_name}</h3>
-          {/* <a href={item.url}>{item.title}</a> */}
-        </li>
-      ))}
-    </ul>
+    <>
+     <Select options={launchYearList} value={filterYear} onChange={handleChange}>
+     </Select>
+     <Select options={rocketNameList} />
+     <Select options={launchSuccessList} />
+     
+     <Table>
+      <thead>
+        <tr>
+          <th>flight_number</th>
+          <th>mission_name</th>
+          <th>launch year</th>
+          <th>rocket name</th>
+          <th>launch success</th>
+        </tr>
+      </thead>
+      <tbody>
+      {data.filter(item => item.launch_year.includes(filterYear)).map((launche) => {
+          return(
+            <tr>
+                <th scope="row">{launche.flight_number}</th>
+                <td>{launche.mission_name}</td>
+                <td>{launche.launch_year}</td>
+                <td>{launche.rocket.rocket_name}</td>
+                <td>{launche.launch_success ? 'success': 'unsuccess'}</td>
+            </tr>
+          )
+        //  console.log(filterYear)
+     })}
+          {/* {data.map((launche) => {
+              return(
+                <tr>
+                    <th scope="row">{launche.flight_number}</th>
+                    <td>{launche.mission_name}</td>
+                    <td>{launche.launch_year}</td>
+                    <td>{launche.rocket.rocket_name}</td>
+                    <td>{launche.launch_success ? 'success': 'unsuccess'}</td>
+                </tr>
+              )
+          })} */}
+      </tbody>
+    </Table>
+    
+    {/* <ul>
+     {data.map(item => (
+         <li key={item.objectID}>
+             <h3>{item.mission_name}</h3>
+           <a href={item.url}>{item.title}</a>
+         </li>
+       ))}
+     </ul> */}
+     </>
   )
 } 

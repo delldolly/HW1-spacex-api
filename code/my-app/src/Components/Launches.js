8d3e6ee54fc5acdb6e7de2d8e20d4c83e69ad89d
Memory <memoryeamkijkarn@gmail.com>
2021-02-17 19:03:30 +0700

    rocket query data

--- code/my-app/src/Components/Launches.js
+++ code/my-app/src/Components/Launches.js
@@ -1,7 +1,34 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
+
 
 export default function Launches(){
-    return(
-        <h1>Launch Detail</h1>
-    )
+    // const response = await fetch('https://api.spacexdata.com/v3/rockets')
+    // const data = await response.json()
+    // console.log(data)
+    const [data, setData] = useState([]);
+    
+    useEffect(() => {
+    const fetchData = async () => {
+      const result = await axios(
+        // 'https://hn.algolia.com/api/v1/search?query=redux',
+        'https://api.spacexdata.com/v3/Launches',
+      );
+        console.log(result.data.hits)
+      setData(result.data);
+    };
+ 
+    fetchData();
+  }, []);
+    
+  return (
+    <ul>
+      {data.map(item => (
+        <li key={item.objectID}>
+            <h3>{item.mission_name}</h3>
+          {/* <a href={item.url}>{item.title}</a> */}
+        </li>
+      ))}
+    </ul>
+  )
 } 
--- code/my-app/src/Components/Rockets.js
+++ code/my-app/src/Components/Rockets.js
@@ -1,34 +1,43 @@
 import React, {useState, useEffect} from 'react';
 import axios from 'axios';
+import '../styles/RocketsPage.css'
+import falcon9 from '../img/falcon_9.jpg'
+import falconHeavy from '../img/falcon_heavy.jpg'
+import { string } from 'prop-types';
 
+const imageList = [falcon9, falconHeavy, falcon9, falconHeavy]
 
 export default function Rockets(){
-    // const response = await fetch('https://api.spacexdata.com/v3/rockets')
-    // const data = await response.json()
-    // console.log(data)
     const [data, setData] = useState([]);
-    
     useEffect(() => {
-    const fetchData = async () => {
-      const result = await axios(
-        // 'https://hn.algolia.com/api/v1/search?query=redux',
-        'https://api.spacexdata.com/v3/rockets',
-      );
-        console.log(result.data.hits)
-      setData(result.data);
-    };
- 
-    fetchData();
-  }, []);
-    
-  return (
-    <ul>
-      {data.map(item => (
-        <li key={item.objectID}>
-            <h3>{item.rocket_name}</h3>
-          {/* <a href={item.url}>{item.title}</a> */}
-        </li>
-      ))}
-    </ul>
+        const fetchData = async () => {
+        const result = await axios(
+            'https://api.spacexdata.com/v3/rockets',
+        );
+        setData(result.data);
+        console.log(data)
+        };
+        fetchData();
+    }, []);
+
+  return (<>
+    {data.map((item, index) => {
+        return(
+            <div className="rocketImg" style={{ backgroundImage: `url(${imageList[index]})` }}>
+                <div className='inner-left-middle'>
+                    <h1>{item.rocket_name}</h1>
+                    <h2>DETAIL</h2>
+                    <p>HEIGHT {item.height.meters} m / {item.height.feet} ft</p>
+                    <p>DIAMETER {item.diameter.meters} m / {item.diameter.feet} ft</p>
+                    <p>MASS {item.mass.kg} kg / {item.mass.lb} lb</p>
+                    {item.payload_weights.map((payload) => {
+                        return (<p>PAY LOAD TO {payload.id.toUpperCase()} {payload.kg} kg / {payload.lb} lb</p>)
+                    })}
+                </div>
+            </div>
+        )    
+    })}
+    </>
   )
 } 
+

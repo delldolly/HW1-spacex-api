9a18c2e799409f99d86b3ba05c3fffbde421a6ff
Memory <memoryeamkijkarn@gmail.com>
2021-02-19 20:52:48 +0700

    debug LaunchDetail and RocketsDetail
    bug : reload page data undefined

--- code/my-app/src/Components/LaunchDetail.js
+++ code/my-app/src/Components/LaunchDetail.js
@@ -3,9 +3,33 @@ import '../styles/LaunchDetails.css';
 import {useParams, Link} from 'react-router-dom';
 import axios from 'axios';
 
+function RenderPage(props){
+    const data = props.listData;
+    console.log(props)
+    return(
+        <>
+            <h1>{data.flight_number} : {data.mission_name}</h1>
+            {/* {data.mission_id.map((MID) => {return(<p>mission id{MID}</p>)})} */}
+            <h4>launch_year : {data.launch_year}</h4>
+            <h6>date : {data.launch_date_local}</h6>
+            <h6>{data.launch_site.site_id}</h6>
+            <h6>{data.launch_site.site_name}</h6>
+            <h6>{data.launch_site.site_name_long}</h6>
+            <h6>launch status : {(data.launch_success? "success":"unsuccess")}</h6>
+            <h5>detail</h5>
+            <h6>{data.details}</h6>
+            <h6>{data.static_fire_date_utc}</h6>
+            <h1>ROCKET </h1>
+            <Link to={"/RocketsDetail/"+(data.rocket.rocket_id)}><h6>name : {data.rocket.rocket_name}</h6></Link>
+            <h6>type : {data.rocket.rocket_type}</h6>
+        </>
+    )
+}
+
 export default function LauncheDetail(){
     const {id} = useParams();
     const [data, setData] = useState(false);
+    const [LOADSTATUS, SETLOADSTATUS] = useState(true);
     useEffect(() => {
         const fetchData = async () => {
             const result = await axios(
@@ -14,26 +38,13 @@ export default function LauncheDetail(){
             setData(result.data);
             console.log(result.data)
             console.log(data.flight_number)
+            SETLOADSTATUS(false)
         };
         fetchData();
     }, []);
-    return(
+    return (
         <>
-            <h1>{data?data.flight_number:"loading"} : {data?data.mission_name:'loading'}</h1>
-            {/* {data.mission_id.map((MID) => {return(<p>mission id{MID}</p>)})} */}
-            <h4>launch_year : {data?data.launch_year:"loading"}</h4>
-            <h6>date : {data?data.launch_date_local:"loading"}</h6>
-            <h6>{data?data.launch_site.site_id:"loading"}</h6>
-            <h6>{data?data.launch_site.site_name:"loading"}</h6>
-            <h6>{data?data.launch_site.site_name_long:"loading"}</h6>
-            <h6>launch status : {data?(data.launch_success? "success":"unsuccess"):"loading"}</h6>
-            <h5>detail</h5>
-            <h6>{data.details}</h6>
-            <h6>{data.static_fire_date_utc}</h6>
-            <h1>ROCKET </h1>
-            <Link to={"/RocketsDetail/"+(data?data.rocket.rocket_id:"1")}><h6>name : {data?data.rocket.rocket_name:"loading name..."}</h6></Link>
-            <h6>type : {data?data.rocket.rocket_type:"loading type..."}</h6>
+        {LOADSTATUS? "Loading...": <RenderPage listData={data}/>}
         </>
-        
-    )
+        )
 }
--- code/my-app/src/Components/RocketsDetail.js
+++ code/my-app/src/Components/RocketsDetail.js
@@ -1,26 +1,39 @@
-import React, {useState, useEffect} from "react";
+import React, {useState, useEffect, useMemo} from "react";
 import {useParams} from 'react-router-dom';
 import axios from 'axios';
 
-const RocketsDetail = () => {
-    const {id} = useParams();
-    const [data, setData] = useState([]);
-    useEffect(() => {
-        const fetchData = async () => {
-            const result = await axios(
-                'https://api.spacexdata.com/v3/rockets/'+id,
-            );
-            setData(result.data);
-            console.log(data)
-        };
-        fetchData();
-    }, []);
+const RenderPage = (props) => {
+    const data = props.listData;
     return(
         <>
-        <h1>{data.id}</h1>
-        <h1>{data.rocket_name}</h1>
+        <h3>ID : {data.id}</h3>
+        <h1>NAME : {data.rocket_name}</h1>
+        <h6>rocket id : {data.rocket_id}</h6>
         </>
     )
 }
 
-export default RocketsDetail;
+export default function RocketsDetail(){
+    const {id} = useParams();
+    const [data, setData] = useState();
+    const [LOADSTATUS, SETLOADSTATUS] = useState(true);
+    useEffect(() => {
+        const fetchData = async () => {
+        const result = await axios(
+            'https://api.spacexdata.com/v3/rockets/'+id,
+        );
+        setData(result.data);
+        SETLOADSTATUS(false)
+        console.log(result.data)
+        console.log(data)
+    };
+        
+        fetchData();
+    }, []);
+    return(
+        <>
+            {LOADSTATUS? "loading...": <RenderPage listData={data}/>}
+        </>
+    )
+}
+
